fdc91ffb492550f859fdb43bbe23998dc9c21a2d
raihanjs <rayhangazi1024@gmail.com>
2024-06-06 21:50:31 +0600

    dynamic meta pixel added

--- src/Pages/Auth/SignIn/SignIn.jsx
+++ src/Pages/Auth/SignIn/SignIn.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../../Hooks/useAuth";
 import GoogleButton from "../../../Components/GoogleButton";
+import { Helmet } from "react-helmet";
 
 export default function SignIn() {
   const { signInUser } = useAuth();
@@ -21,8 +22,35 @@ export default function SignIn() {
         console.log(error);
       });
   };
+
+  const scriptContent = !(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(
+    window,
+    document,
+    "script",
+    "https://connect.facebook.net/en_US/fbevents.js"
+  );
+  fbq("init", "1494823334765932");
+  fbq("track", "PageView");
   return (
     <section className="container">
+      <Helmet>
+        <script type="text/javascript">{scriptContent}</script>
+      </Helmet>
       <div className="hero min-h-screen">
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,8 +3,35 @@ import HomeBanner from "./HomeBanner/HomeBanner";
 import HomeServices from "./HomeServices/HomeServices";
 
 export default function HomePage() {
+  const scriptContent = !(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(
+    window,
+    document,
+    "script",
+    "https://connect.facebook.net/en_US/fbevents.js"
+  );
+  fbq("init", "1000704068224420");
+  fbq("track", "PageView");
+
   return (
     <>
+      <Helmet>
+        <script type="text/javascript">{scriptContent}</script>
+      </Helmet>
       <HomeBanner />
       <HomeServices />
     </>
