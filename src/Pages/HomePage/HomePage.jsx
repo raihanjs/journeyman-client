6d4053f49ce5c3936adbf3e96d7f56fa86387a33
raihanjs <rayhangazi1024@gmail.com>
2024-06-09 12:17:33 +0600

    my pixel 2 setup

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,23 +4,23 @@ import HomeServices from "./HomeServices/HomeServices";
 import { Helmet } from "react-helmet";
 
 export default function HomePage() {
-  //   const scriptContent = `!function(f,b,e,v,n,t,s)
-  // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  // n.queue=[];t=b.createElement(e);t.async=!0;
-  // t.src=v;s=b.getElementsByTagName(e)[0];
-  // s.parentNode.insertBefore(t,s)}(window, document,'script',
-  // 'https://connect.facebook.net/en_US/fbevents.js');
-  // fbq('init', '982753329723910');
-  // fbq('track', 'PageView');
-  // `;
+  const scriptContent = `!function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '982753329723910');
+  fbq('track', 'PageView');
+  `;
 
   return (
     <>
-      {/* <Helmet>
+      <Helmet>
         <script type="text/javascript">{scriptContent}</script>
-      </Helmet> */}
+      </Helmet>
       <HomeBanner />
       <HomeServices />
     </>
