da28b1d76ef43290dff53db9b11ec84836cc13cb
raihanjs <rayhangazi1024@gmail.com>
2024-06-09 12:22:36 +0600

    removed fb pixel code

--- src/Pages/Auth/SignIn/SignIn.jsx
+++ src/Pages/Auth/SignIn/SignIn.jsx
@@ -23,34 +23,8 @@ export default function SignIn() {
       });
   };
 
-  const scriptContent = `!(function (f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = !0;
-    n.version = "2.0";
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = !0;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s);
-  })(
-    window,
-    document,
-    "script",
-    "https://connect.facebook.net/en_US/fbevents.js"
-  );
-  fbq("init", "1494823334765932");
-  fbq("track", "PageView");`;
   return (
     <section className="container">
-      <Helmet>
-        <script type="text/javascript">{scriptContent}</script>
-      </Helmet>
       <div className="hero min-h-screen">
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
